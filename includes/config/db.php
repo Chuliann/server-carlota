48acdde3ceef983165c40c9aa60cc7916d97e1d4
Julian Berton <julianbertonb@gmail.com>
2023-01-17 18:27:34 -0300

    datos actualizados

--- includes/config/db.php
+++ includes/config/db.php
@@ -4,8 +4,8 @@ include __DIR__ . "/../../variables.php";
 
 function conectarDB(): mysqli
 {
-    $db = new mysqli('localhost', 'root', '', 'carlota');
-   /*  $db = new mysqli('localhost', 'carlotai_julian', 'UnAcOnTrAdIfIcIl', 'carlotai_website'); */
+    /* $db = new mysqli('localhost', 'root', '', 'carlota'); */
+    $db = new mysqli('localhost', 'carlotai_julian', 'UnAcOnTrAdIfIcIl', 'carlotai_website');
     
     if (!$db) {
         echo "Error, no se pudo conectar";
